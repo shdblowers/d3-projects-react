22c618086a00054b201b1a055c4f30c6ba99bdc5
Steven Blowers <sblowers@findmypast.com>
2017-12-08 17:09:14 +0000

    responsive to screen height width changes

--- src/App.js
+++ src/App.js
@@ -23,6 +23,24 @@ const colourScale = scaleThreshold()
   .range(['#75739F', '#5EAFC6', '#41A368', '#93C464']);
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.onResize = this.onResize.bind(this);
+    this.state = { screenWidth: 100, screenHeight: 500 };
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize, false);
+    this.onResize();
+  }
+
+  onResize() {
+    this.setState({
+      screenWidth: window.innerWidth - 20,
+      screenHeight: window.innerHeight - 120,
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -33,19 +51,17 @@ class App extends Component {
           <StreamGraph
             colourScale={colourScale}
             data={appData}
-            size={[1000, 250]}
+            size={[this.state.screenWidth, this.state.screenHeight / 2]}
           />
-        </div>
-        <div>
           <WorldMap
             colourScale={colourScale}
             data={appData}
-            size={[500, 400]}
+            size={[this.state.screenWidth / 2, this.state.screenHeight / 2]}
           />
           <BarChart
             colourScale={colourScale}
             data={appData}
-            size={[500, 400]}
+            size={[this.state.screenWidth / 2, this.state.screenHeight / 2]}
           />
         </div>
       </div>
